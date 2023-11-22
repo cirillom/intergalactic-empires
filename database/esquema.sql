dd196aa9470faab24f3e7331cfd138397fceb5ef
Matheus Cirillo <matheuscirillo@usp.br>
2023-11-22 11:08:53 -0300

    added all fields to all entities with foreign keys

--- database/esquema.sql
+++ database/esquema.sql
@@ -157,12 +157,16 @@
         -- int qtd;*
     
     -- colonia
+        -- string IMPERIO; <- chave estrangeira (IMPERIO) <- chave primaria
+        -- string PLANETA; <- chave estrangeira (PLANETA) <- chave primaria
+        -- int turno_inicial; <- chave primaria
+        -- int turno_final;
 
     -- Batalha
         -- int ID; <- chave primaria (autogerado)
         -- string IMPÉRIO; <- chave estrangeira (COLONIA) <- unique
         -- string PLANETA_ATACANTE; <- chave estrangeira (COLONIA) <- unique
-        -- string TURNO; <- chave estrangeira (COLONIA) <- unique
+        -- int TURNO; <- chave estrangeira (COLONIA) <- unique
         -- string PLANETA_DEFENSOR; <- chave estrangeira (PLANETA) <- unique
         -- int turno_batalha;* <- unique
         -- string PLANETA_VENCEDOR; <- chave estrangeira (PLANETA)
@@ -171,33 +175,120 @@
         -- float poderio_defensor;*
     
     -- estoque_gasto_batalha
+        -- int ID_BATALHA; <- chave estrangeira (BATALHA) <- chave primaria
+        -- string PLANETA; <- chave estrangeira (PLANETA) <- chave primaria
+        -- string RECURSO; <- chave estrangeira (RECURSO) <- chave primaria
+        -- int turno; <- chave primaria
+        -- int qtd*; 
 
     -- Exploracao
+        -- IMPÉRIO; <- chave estrangeira (COLONIA) <- chave primaria
+        -- PLANETA_EXPLORADOR; <- chave estrangeira (COLONIA) <- chave primaria
+        -- TURNO_INICIAL; <- chave estrangeira (COLONIA) <- chave primaria
+        -- PLANETA_EXPLORADO; <- chave estrangeira (PLANETA) <- chave primaria
+        -- turno; <- chave primaria
+        -- incerteza*;
+        -- poderio militar; 
+        -- atmosfera;
+        -- qtd população; 
+        -- qtd estruturas;
+        -- qtd água;
+        -- gravidade;
+        -- temperatura;
+        -- fertilidade;
 
     -- Move_recurso
+        -- string IMPERIO; <- chave estrangeira (COLONIA) <- chave primaria
+        -- string PLANETA_DESTINO <- chave estrangeira (COLONIA) <- chave primaria
+        -- int TURNO_INICIAL <- chave estrangeira (COLONIA) <- chave primaria
+        -- string PLANETA_ORIGEM <- chave estrangeira (Estoque) <- chave primaria
+        -- string RECURSO <- chave estrangeira (Estoque) <- chave primaria
+        -- int TURNO <- chave estrangeira (Estoque) <- chave primaria
+        -- int qtd*
+        -- int qtd_naves*
 
     -- recursos_para_estrutura
+        -- string ESTRUTURA <- chave estrangeira (Estrutura) <- chave primaria
+        -- string RECURSO <- chave estrangeira (Recursos) <- chave primaria
+        -- int qtd*
 
     -- prioridade_producao
+        -- PLANETA; <- chave estrangeira (PLANETA) <- chave primaria
+        -- RECURSO; <- chave estrangeira (RECURSO) <- chave primaria
+        -- turno; <- chave primaria
+        -- ordem; <- chave primaria
 
     -- estoque
+        -- PLANETA; <- chave estrangeira (PLANETA) <- chave primaria
+        -- RECURSO;  <- chave estrangeira (RECURSO) <- chave primaria
+        -- turno; <- chave primaria
+        -- quantidade*;
 
     -- recurso_precisa_tecnologia
-
+        -- string NOME;<-chave estrangeira(Tecnologia)<-chave primaria
+        -- int NIVEL;<-chave estrangeira(Tecnologia)<-chave primaria
+        -- string RECURSO;;<-chave estrangeira(Recursos)<-chave primaria
+	
     -- industria
+        -- string ESTRUTURA;<-chave estrangeira(Estrutura)<-chave primaria
+        -- string RECURSO_GERADO*<-chave primaria(Recursos)
 
     -- move_povo
+        -- string IMPERIO;<-chave estrangeira(Colonia)<-chave primaria
+        -- string PLANETA_DESTINO;<-chave estrangeira(Colonia)<-chave primaria
+        -- int TURNO_INICIAL;<-chave estrangeira(Colonia)<-chave primaria
+        -- string PLANETA_ORIGEM;<-chave estrangeira(Povo)<-chave primaria
+        -- string ESPECIE;<-chave estrangeira(Colonia)<-chave primaria
+        -- string TIPO;<-chave estrangeira(Colonia)<-chave primaria
+        -- int TURNO;<-chave estrangeira(Colonia)<-chave primaria
+        -- int qtd*;
+        -- int qtd_naves*;
+	
 
     -- povo_morto_batalha
+        -- int ID_BATALHA;<-chave estrangeira(estoque_gasto_batalha)<-chave primaria
+        -- string PLANETA;<-chave estrangeira(Povo)<-chave primaria
+        -- string ESPECIE;<-chave estrangeira(Povo)<-chave primaria
+        -- string TIPO;<-chave estrangeira(Povo)<-chave primaria
+        -- int TURNO;<-chave estrangeira(Povo)<-chave primaria
+        -- int qtd*
+
+	
 
     -- conhecimento
+        -- string PLANETA;<-chave estrangeira(Povo)<-chave primaria
+        -- string ESPECIE;<-chave estrangeira(Povo)<-chave primaria
+        -- string TIPO*;<-chave estrangeira(Povo)
+        -- int TURNO*;<-chave estrangeira(Povo)
+        -- string NOME;<-chave estrangeira(Tecnologia)<-chave primaria
+        -- int NIVEL;<-chave estrangeira(Tecnologia)<-chave primaria
+	
 
     -- construcao
+        -- string PLANETA;<-chave estrangeira(Prioridade_producao)<-chave primaria
+        -- string ESTRUTURA;<-chave estrangeira(Estrutura)<-chave primaria
+        -- int turno;<chave primaria
+        -- int quantidade*;
 
     -- atuacao
+        -- string PLANETA*;<-chave estrangeira(Construção)
+        -- string ESTRUTURA;<-chave estrangeira(Construção) <-chave primaria
+        -- string TURNO*;<-chave estrangeira(Construção) 
+        -- string PLANETA;<-chave estrangeira(Civil) <-chave primaria
+        -- string ESPECIE;<-chave estrangeira(Civil) <-chave primaria
+        -- int TURNO;<-chave estrangeira(Civil) <-chave primaria
+        -- int qtd_trabalhadores*;
 
     -- gera_recurso
-
+        -- string ESTRUTURA; <-chave estrangeira(Atuação)<- chave primaria
+        -- string PLANETA;<-chave estrangeira(Atuação)<-chave primaria
+        -- string ESPÉCIE;<-chave estrangeira(Atuação)<- chave primaria
+        -- string TURNO;<-chave estrangeira(Atuação)<-chave primaria
+        -- string RECURSO;<- chave estrangeira(Recursos)<-chave primaria
+        -- int qtd*;
+	
     -- civil
-
-
+        -- string PLANETA; <- chave estrangeira(Povo) <- chave primaria
+        -- string ESPECIO; <- chave estrangeira(Povo) <- chave primaria
+        -- string tipo* <- chave estrangeira(Povo)
+        -- int TURNO;<- chave estragengeira(Povo) <-chave primaria
