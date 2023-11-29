01f4aba393896bcda8619aecce15381a8e4c4be9
Matheus Cirillo <matheuscirillo@usp.br>
2023-11-28 22:40:31 -0300

    compiles again

--- database/esquema.sql
+++ database/esquema.sql
@@ -16,7 +16,7 @@
             temperatura_kelvin NUMBER(5) DEFAULT 0 NOT NULL,
             estruturas_max NUMBER(12) DEFAULT 0 NOT NULL,
             gravidade NUMBER(5, 2) DEFAULT 0 NOT NULL,
-            massa NUMBER(12) DEFAULT 0 NOT NULL, -! update this into MR
+            massa NUMBER(12) DEFAULT 0 NOT NULL,
             qtd_agua NUMBER(3,3) DEFAULT 0 NOT NULL,
             fertilidade NUMBER(7) DEFAULT 0 NOT NULL,
             poderio_militar NUMBER(12) DEFAULT 0 NOT NULL,
@@ -83,7 +83,6 @@
             CONSTRAINT CK_PODER_DESTRUTIVO CHECK(poder_destrutivo >= 0),
             CONSTRAINT CK_CAPACIDADE_DE_TRANSPORTE CHECK(capacidade_de_transporte >= 0),
             CONSTRAINT CK_ALCANCE CHECK(alcance >= 0)
-            -NOME É O TIPO DA TECNOLOGIA, PRECISA VERIFICAR EM APLICAÇÃO SE OS VALORES PREENCHIDOS CONDIZEM COM O TIPO
         );
 
         CREATE TABLE ESTRUTURA(
@@ -351,7 +350,34 @@
             qtd NUMBER(12) DEFAULT 0 NOT NULL,
             CONSTRAINT PK_ESTOQUE_GASTO_BATALHA PRIMARY KEY(id_batalha, planeta, recurso, turno),
             CONSTRAINT FK_ESTOQUE_GASTO_BATALHA_ID_BATALHA FOREIGN KEY(id_batalha) REFERENCES BATALHA(id),
-            CONSTRAINT FK_ESTOQUE_GASTO_BATALHA_PLANETA FOREIGN KEY(planeta, recurso) REFERENCES ESTOQUE(planeta, recurso),
+            CONSTRAINT FK_ESTOQUE_GASTO_BATALHA_PLANETA FOREIGN KEY(planeta, recurso, turno) REFERENCES ESTOQUE(planeta, recurso, turno),
             CONSTRAINT CK_ESTOQUE_GASTO_BATALHA_TURNO CHECK(turno >= 0),
             CONSTRAINT CK_ESTOQUE_GASTO_BATALHA_QTD CHECK(qtd >= 0)
         );
+        
+        
+select 'drop table '||table_name||' cascade constraints;' from user_tables;
+    drop table ATUACAO cascade constraints;
+    drop table BATALHA cascade constraints;
+    drop table CIVIL cascade constraints;
+    drop table COLONIA cascade constraints;
+    drop table CONHECIMENTO cascade constraints;
+    drop table CONSTRUCAO cascade constraints;
+    drop table ESPECIE cascade constraints;
+    drop table ESTOQUE cascade constraints;
+    drop table ESTOQUE_GASTO_BATALHA cascade constraints;
+    drop table ESTRUTURA cascade constraints;
+    drop table EXPLORACAO cascade constraints;
+    drop table GERA_RECURSO cascade constraints;
+    drop table IMPERIO cascade constraints;
+    drop table INDUSTRIA cascade constraints;
+    drop table MOVE_POVO cascade constraints;
+    drop table MOVE_RECURSO cascade constraints;
+    drop table PLANETA cascade constraints;
+    drop table POVO cascade constraints;
+    drop table POVO_MORTO_BATALHA cascade constraints;
+    drop table PRIORIDADE_PRODUCAO cascade constraints;
+    drop table RECURSOS cascade constraints;
+    drop table RECURSO_PARA_ESTRUTURA cascade constraints;
+    drop table RECURSO_PRECISA_TECNOLOGIA cascade constraints;
+    drop table TECNOLOGIA cascade constraints;
