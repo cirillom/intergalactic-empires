a05f498f2e819e9498b638e4a86f2b8d5dd754c1
Matheus Cirillo <matheuscirillo@usp.br>
2023-11-30 23:54:49 -0300

    fixed data

--- database/dados/[15] ATUACAO.sql
+++ database/dados/[15] ATUACAO.sql
@@ -4,6 +4,6 @@ INSERT INTO ATUACAO(planeta_construcao, estrutura, turno_construcao, planeta_civ
 INSERT INTO ATUACAO(planeta_construcao, estrutura, turno_construcao, planeta_civil, especie, turno_civil, qtd_trabalhadores) VALUES ('VULCAN', 'LENHA', 5, 'VULCAN', 'VULCANS', 5, 60000);
 INSERT INTO ATUACAO(planeta_construcao, estrutura, turno_construcao, planeta_civil, especie, turno_civil, qtd_trabalhadores) VALUES ('VULCAN', 'PEDREIRA', 5, 'VULCAN', 'VULCANS', 5, 490000);
 INSERT INTO ATUACAO(planeta_construcao, estrutura, turno_construcao, planeta_civil, especie, turno_civil, qtd_trabalhadores) VALUES ('VULCAN', 'MINERADORA', 5, 'VULCAN', 'VULCANS', 5, 80000);
-INSERT INTO ATUACAO(planeta_construcao, estrutura, turno_construcao, planeta_civil, especie, turno_civil, qtd_trabalhadores) VALUES ('VILTRUM', 'LENHA', 5, 'VILTRUIM', 'VILTRUMITA', 5, 900000);
-INSERT INTO ATUACAO(planeta_construcao, estrutura, turno_construcao, planeta_civil, especie, turno_civil, qtd_trabalhadores) VALUES ('VILTRUM', 'PEDREIRA', 5, 'VILTRUIM', 'VILTRUMITA', 5, 1750000);
-INSERT INTO ATUACAO(planeta_construcao, estrutura, turno_construcao, planeta_civil, especie, turno_civil, qtd_trabalhadores) VALUES ('VILTRUM', 'FABRICA DE CHIP', 5, 'VILTRUIM', 'VILTRUMITA', 5, 750000);
+INSERT INTO ATUACAO(planeta_construcao, estrutura, turno_construcao, planeta_civil, especie, turno_civil, qtd_trabalhadores) VALUES ('VILTRUM', 'LENHA', 5, 'VILTRUM', 'VILTRUMITA', 5, 900000);
+INSERT INTO ATUACAO(planeta_construcao, estrutura, turno_construcao, planeta_civil, especie, turno_civil, qtd_trabalhadores) VALUES ('VILTRUM', 'PEDREIRA', 5, 'VILTRUM', 'VILTRUMITA', 5, 1750000);
+INSERT INTO ATUACAO(planeta_construcao, estrutura, turno_construcao, planeta_civil, especie, turno_civil, qtd_trabalhadores) VALUES ('VILTRUM', 'FABRICA DE CHIP', 5, 'VILTRUM', 'VILTRUMITA', 5, 750000);
---
+++ database/dados/[16] GERA_RECURSO.sql
@@ -0,0 +1,9 @@
+INSERT INTO GERA_RECURSO(id_atuacao, recurso, qtd) VALUES (1, 'MADEIRA', 315000);
+INSERT INTO GERA_RECURSO(id_atuacao, recurso, qtd) VALUES (2, 'PEDRA', 1225000);
+INSERT INTO GERA_RECURSO(id_atuacao, recurso, qtd) VALUES (3, 'MINERIO', 70000);
+INSERT INTO GERA_RECURSO(id_atuacao, recurso, qtd) VALUES (4, 'MADEIRA', 37800);
+INSERT INTO GERA_RECURSO(id_atuacao, recurso, qtd) VALUES (5, 'PEDRA', 308700);
+INSERT INTO GERA_RECURSO(id_atuacao, recurso, qtd) VALUES (6, 'MINERIO', 50400);
+INSERT INTO GERA_RECURSO(id_atuacao, recurso, qtd) VALUES (7, 'MADEIRA', 828000);
+INSERT INTO GERA_RECURSO(id_atuacao, recurso, qtd) VALUES (8, 'PEDRA', 1610000);
+INSERT INTO GERA_RECURSO(id_atuacao, recurso, qtd) VALUES (9, 'CHIP', 690000);
