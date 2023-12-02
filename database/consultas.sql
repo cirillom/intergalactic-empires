e6b0263a9c0b8eff00691bb09763a1093ebbc043
Matheus Cirillo <matheuscirillo@usp.br>
2023-12-02 18:09:22 -0300

    3 consultas funcionando
    plano de geração de mais 3 consultas

--- database/consultas.sql
+++ database/consultas.sql
@@ -1,58 +1,44 @@
 -- Quantidade de recursos de um império em um turno
--- get all colonias from an empire where turno_inicial is less than the desired turn and turno_final is geq than the desired turn
--- get estoque at desired turn from each of the planets in colonias
--- group by resource
-VARIABLE turno_atual NUMBER;
-
-EXEC :turno_atual := 3;
-
 SELECT
     E.RECURSO,
-    SUM(E.QUANTIDADE) AS QUANTIDADE
+    NVL(SUM(E.QUANTIDADE), 0) AS QUANTIDADE
 FROM
     ESTOQUE E
     JOIN COLONIA C
-    ON E.PLANETA = C.PLANETA
+        ON E.PLANETA = C.PLANETA
+    RIGHT JOIN RECURSOS R
+        ON E.RECURSO = R.NOME
 WHERE
-    C.IMPERIO = 'MONGOL'
-    AND C.TURNO_INICIAL <= :TURNO_ATUAL
-    AND (C.TURNO_FINAL >= :TURNO_ATUAL
-    OR C.TURNO_FINAL IS NULL)
+    C.IMPERIO = :IMPERIO AND C.TURNO_INICIAL <= :TURNO_ATUAL AND (C.TURNO_FINAL >= :TURNO_ATUAL OR C.TURNO_FINAL IS NULL)
     AND E.TURNO = :TURNO_ATUAL
 GROUP BY
     E.RECURSO
 ORDER BY
     E.RECURSO;
 
-EXEC :turno_atual := 5;
-
 -- Quantidade de recursos gerados por um império (soma de tudo gerado em todas as atuações)
--- get all colonias from an empire (including past ones)
--- get all atuacoes from the planets in colonias from turno_inicial to turno_final or current turn if turno_final is null
--- get all gera_recursos from the atuacoes where estruturas is of type industria (maybe atuacoes should have ID?)
--- group by resource summing quantity
--- (another query to check for life generation?)
 SELECT
-    G.RECURSO,
-    SUM(G.QTD)
+    R.NOME AS RECURSO,
+    NVL(SUM(G.QTD), 0) AS QUANTIDADE
 FROM
     GERA_RECURSO G
     JOIN ATUACAO A
-    ON G.ID_ATUACAO = A.ID JOIN ESTRUTURA E
-    ON E.NOME = A.ESTRUTURA
+        ON G.ID_ATUACAO = A.ID
     JOIN COLONIA C
-    ON C.PLANETA = A.PLANETA_CIVIL
-WHERE
-    E.TIPO = 'INDUSTRIA'
-    AND C.IMPERIO = 'MONGOL'
-    AND C.TURNO_INICIAL <= :TURNO_ATUAL
-    AND (C.TURNO_FINAL >= :TURNO_ATUAL
-    OR C.TURNO_FINAL IS NULL)
-    AND A.TURNO_CIVIL = :TURNO_ATUAL
+        ON C.PLANETA = A.PLANETA_CIVIL AND C.TURNO_INICIAL <= A.TURNO_CIVIL AND (C.TURNO_FINAL >= A.TURNO_CIVIL OR C.TURNO_FINAL IS NULL)
+        AND IMPERIO = :IMPERIO
+    RIGHT JOIN RECURSOS R
+        ON G.RECURSO = R.NOME
 GROUP BY
-    G.RECURSO
+    R.NOME
 ORDER BY
-    G.RECURSO;
+    R.NOME;
+
+-- Quantidade de civis transformados de cada especie por um império
+-- Especie | qtd
+-- Humanos | 25980
+-- Navi | 0
+-- [...]
 
 -- Quantidade de individuos de cada especie morto nas batalhas de um império
 SELECT
@@ -68,6 +54,6 @@ FROM
         E.NOME;
 
 
--- o planeta A consegue construir a estrutura X com o estoque que ele tem
+-- o planeta A consegue construir a estrutura X com o estoque que ele tem (divisão relacional)
 
 -- quantas construções um povo atuou
